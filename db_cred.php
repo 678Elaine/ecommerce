55d70276b7f8b9e845ca62b4dc4b07836e66e0db
678Elaine <94685092+678Elaine@users.noreply.github.com>
2022-11-13 22:53:55 +0000

    Update db_cred.php

--- db_cred.php
+++ db_cred.php
@@ -1,8 +1,8 @@
 <?php
 //Database credentials
-define("servername","localhost");
-define("username","root");
-define("password","");
+define("servername","us-cdbr-east-06.cleardb.net");
+define("username","b8d1db40e6cbf6");
+define("password","5b8e4dca");
 define("dbname","heroku_2c41afb6f1ac824");
 
 ?>
