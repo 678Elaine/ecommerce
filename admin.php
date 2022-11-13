a033997e7c98669e7fcda94af2eb0bc9b00e68a4
678Elaine <94685092+678Elaine@users.noreply.github.com>
2022-11-13 03:29:28 +0000

    Update admin.php

--- admin.php
+++ admin.php
@@ -1,6 +1,6 @@
-<button type="submit" name ="login"><a href= "../actions/add_brand.php"> Add a brand </a></button>
-<button type="submit" name ="login"><a href= "../actions/update_brand.php"> Update a brand </a></button>
-<button type="submit" name ="login"><a href= "../actions/add_category.php"> Add a Category</a></button>
-<button type="submit" name ="login"><a href= "../actions/update_category.php"> Update a Category </a></button>
-<button type="submit" name ="login"><a href= "../view/addproduct.php"> Add a product </a></button>
-<button type="submit" name ="login"><a href= "../actions/Update_product.php"> Update a product </a></button>
+<button type="submit" name ="login"><a href= "add_brand.php"> Add a brand </a></button>
+<button type="submit" name ="login"><a href= "update_brand.php"> Update a brand </a></button>
+<button type="submit" name ="login"><a href= "add_category.php"> Add a Category</a></button>
+<button type="submit" name ="login"><a href= "update_category.php"> Update a Category </a></button>
+<button type="submit" name ="login"><a href= "addproduct.php"> Add a product </a></button>
+<button type="submit" name ="login"><a href= "Update_product.php"> Update a product </a></button>
